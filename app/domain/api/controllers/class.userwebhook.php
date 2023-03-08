15b8e0754f99d2dfc9fd26bd859356ec0b1412df
tiggomark <thomazrdamasceno@gmail.com>
2023-03-08 08:33:20 +0000

    improve user api creation

--- app/domain/api/controllers/class.userwebhook.php
+++ app/domain/api/controllers/class.userwebhook.php
@@ -34,41 +34,47 @@ namespace tiggomark\domain\controllers {
 
             header('Content-Type: application/json');
 
+            try {
+
+                 $json_payload = file_get_contents('php://input');
+                            $params = json_decode($json_payload, true);
+
+                            $values = array(
+                                'email'         => "",
+                                'tenant'         => "",
+                                'password'      => "",
+                                'firstname'     => "",
+                                'lastname'      => "",
+                                'tenant'      => ""
+                            );
 
 
-            $json_payload = file_get_contents('php://input');
-            $params = json_decode($json_payload, true);
-
-            $values = array(
-                'email'         => "",
-                'tenant'         => "",
-                'password'      => "",
-                'firstname'     => "",
-                'lastname'      => "",
-                'tenant'      => ""
-            );
-
-
-            $values = array(
-                'email' => ($params['email']),
-                'password' => $params['password'],
-                'firstname' => ($params['name']),
-                'company' => ($params['companyName']),
-                 'tenant' => ($params['tenant'])
-            );
+                            $values = array(
+                                'email' => ($params['email']),
+                                'password' => $params['password'],
+                                'firstname' => ($params['name']),
+                                'lastname'      => "",
+                                'company' => ($params['companyName']),
+                                 'tenant' => ($params['tenant'])
+                            );
 
 
 
+                if ($this->installRepo->setupDB($values)) {
 
-            if ($this->installRepo->setupDB($values)) {
+                   http_response_code(200);
+                  echo '{"status": "success"}';
+                } else {
+                   http_response_code(500);
+                   $response = array('error' => 'Ocorreu um erro ao criar o usuário ');
+                                     echo json_encode($response);
+                }
 
-               http_response_code(200);
-            } else {
-
+            } catch (Exception $e) {
                http_response_code(500);
+                  $response = array('error' => 'Ocorreu um erro: ' . $e->getMessage());
+                  echo json_encode($response);
             }
-
-
 
 
         }
